46e3a25e694dfc5cd703eb46c27d4b6ead648f3d
Adam Graham <adam@zigurous.com>
2026-02-13 21:42:54 -0600

    Reset material keywords when creating new materials

--- Runtime/MaterialSelectorRenderer.cs
+++ Runtime/MaterialSelectorRenderer.cs
@@ -119,6 +119,10 @@ namespace Zigurous.Prototyping
             material.SetInt(_DoubleSidedEnable, m_DoubleSided ? 1 : 0);
             material.doubleSidedGI = m_DoubleSided;
 
+            #if UNITY_EDITOR
+            UnityEditor.Rendering.HighDefinition.HDShaderUtils.ResetMaterialKeywords(material);
+            #endif
+
             return material;
         }
 
