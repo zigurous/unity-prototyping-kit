094de7a442c4596674a0e4359f4100e8b30dea02
Adam Graham <adam@zigurous.com>
2026-09-23 13:34:07 -0500

    Add SelectionBase script

---
+++ Runtime/SelectionBase.cs
@@ -0,0 +1,13 @@
+using UnityEngine;
+
+namespace Zigurous.Prototyping
+{
+    /// <summary>
+    /// Marks the GameObject as a selection base object for Scene View picking.
+    /// </summary>
+    [SelectionBase]
+    [AddComponentMenu("Zigurous/Prototyping/Selection Base")]
+    [HelpURL("https://docs.zigurous.com/com.zigurous.prototyping/api/Zigurous.Prototyping/SelectionBase")]
+    public class SelectionBase : MonoBehaviour {}
+
+}
